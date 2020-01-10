4d39d7cb958bdb98b73f2035e22aeab76460083d
Treagan Birbal <treaganbirbal@pursuit.org>
2020-01-10 14:14:29 -0500

    sll push method implementation

--- linked_lists.js
+++ linked_lists.js
@@ -5,24 +5,43 @@ class Node{
     }
 }
 
-class singlyLinkedList {
+class SinglyLinkedList {
     constructor(){
         this.head = null;
         this.tail = null
         this.length = 0
     }
+    push(val){
+        const addedNode = new Node(val)
+        if(!this.head){
+            this.head = addedNode
+            this.tail = addedNode
+        }
+        else{
+            this.tail.next = addedNode
+            this.tail = addedNode;
+        }
+        this.length++
+        return this
+    }
+
+    pop
 }
 
-let newNode1 = new Node(1)
-let newNode2 = new Node(2)
-let newNode3 = new Node(3)
-let newNode4 = new Node(4)
-let newNode5 = new Node(5)
+// let newNode1 = new Node(1)
+// let newNode2 = new Node(2)
+// let newNode3 = new Node(3)
+// let newNode4 = new Node(4)
+// let newNode5 = new Node(5)
 
-newNode1.next = newNode2
-newNode2.next = newNode3
-newNode3.next = newNode4
-newNode4.next = newNode5
+// newNode1.next = newNode2
+// newNode2.next = newNode3
+// newNode3.next = newNode4
+// newNode4.next = newNode5
+
+const sll = new SinglyLinkedList();
+
+sll.push(1).push(2).push(4)
 
 
-console.log(newNode1)
+console.log(sll)
