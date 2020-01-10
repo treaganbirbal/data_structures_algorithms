9bc9f8e64933e1da3211cbd749f502731d1d3f28
Treagan Birbal <treaganbirbal@pursuit.org>
2020-01-10 13:03:46 -0500

    create SLL class. create 5 nodes

--- linked_lists.js
+++ linked_lists.js
@@ -1,6 +1,23 @@
 class Node{
     constructor(value){
         this.value = value;
-        this.next = next
+        this.next = null
     }
 }
+
+class singlyLinkedList {
+    constructor(){
+        this.head = null;
+        this.tail = null
+        this.length = 0
+    }
+}
+
+let newNode1 = new Node(1)
+let newNode2 = new Node(2)
+let newNode3 = new Node(3)
+let newNode4 = new Node(4)
+let newNode5 = new Node(5)
+
+
+console.log(newNode5)
