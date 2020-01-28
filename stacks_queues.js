5847168596aa6f9cf797dad67d80a361c0623604
Treagan Birbal <treaganbirbal@pursuit.org>
2020-01-28 10:37:54 -0500

    implement Node class, Stack class & push method

--- stacks_queues.js
+++ stacks_queues.js
@@ -1,4 +1,51 @@
-class Stack{
-    constructor()
+class Node{
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
 }
+
+
+class Stack{
+  constructor(){
+      this.length = 0;
+      this.top = null
+  }
+
+  push(value){
+    const newNode = new Node(value)
+
+    if(!this.length){
+        this.top = newNode
+    } else {
+        newNode.next = this.top;
+        this.top = newNode
+    }
+    this.length += 1
+    return newNode
+  }
+
+  pop(){
+      if(!this.length){
+          return null
+      } else {
+          let returnedNode = this.top
+          returnedNode.next = this.top
+          this.top = returnedNode.next
+          return returnedNode
+      }
+      this.length--
+  }
+}
+
+let stack = new Stack()
+
+stack.push(1)
+stack.push(2)
+stack.push(3)
+stack.push(4)
+stack.pop()
+console.log(stack)
+
+
 
