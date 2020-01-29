d756a5d61b6f498d5e9f7852db23d14ffb117833
Treagan Birbal <treaganbirbal@pursuit.org>
2020-01-28 23:21:05 -0500

    implement enqueue with nodes already in queue

--- queues.js
+++ queues.js
@@ -17,13 +17,25 @@ class Queue{
         if(!this.length){
             this.first = newNode
             this.last = newNode
-
+        } else {
+            this.last.next = newNode
+            this.last = newNode
         }
         this.length+=1
         return newNode
     }
+
+    // dequeue(){
+    //     if(!this.length){
+    //         return null
+    //     }
+    //     else {
+
+    //     }
+    // }
 }
 
 let q = new Queue()
 q.enqueue(1)
+console.log(q.enqueue(2))
 console.log(q)
