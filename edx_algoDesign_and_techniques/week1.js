f5a0e2e2e0b861653b72853d55a4485c99fd76fe
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-04 12:22:37 -0500

    rm console.log

--- edx_algoDesign_and_techniques/week1.js
+++ edx_algoDesign_and_techniques/week1.js
@@ -9,7 +9,6 @@
 //1.1  Implementing an AlgorithmFor this trivial problem, we will skip “Designing an algorithm” step andwill move right to the pseudocode.SumOfTwoDigits(a,b):returna+bSince the pseudocode does not specify how we inputaandb, below weprovide solutions inC++,Java, andPython3programming languages aswell as recommendations on compiling and running them. You can copy-and-paste the code to a file, compile/run it, test it on a few datasets, andthen submit (the source file, not the compiled executable) to the gradingsystem.  Needless to say,  we assume that you know the basics of one ofprogramming languages that we use in our grading system
 
 const twoSum = (a, b) => a + b;
-// console.log(twoSum(1,6))
 
 //********/
 
