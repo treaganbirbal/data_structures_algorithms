b9f2300c97535bfdfcbb1f34d8bed39226c381b9
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-04 12:41:05 -0500

    begin maxPairWiseProduct"

--- edx_algoDesign_and_techniques/week1.js
+++ edx_algoDesign_and_techniques/week1.js
@@ -8,11 +8,23 @@
 
 //1.1  Implementing an AlgorithmFor this trivial problem, we will skip “Designing an algorithm” step andwill move right to the pseudocode.SumOfTwoDigits(a,b):returna+bSince the pseudocode does not specify how we inputaandb, below weprovide solutions inC++,Java, andPython3programming languages aswell as recommendations on compiling and running them. You can copy-and-paste the code to a file, compile/run it, test it on a few datasets, andthen submit (the source file, not the compiled executable) to the gradingsystem.  Needless to say,  we assume that you know the basics of one ofprogramming languages that we use in our grading system
 
-const twoSum = (a, b) => a + b;
+// const twoSum = (a, b) => a + b;
 
 //********/
 
 // 2.   Maximum Pairwise Product - Find the maximum product of two distinct num-bers in a sequence of non-negative integers.
 
-/
+//input: 
+const input = [[10], 
+                [7, 5, 14, 2, 8, 8, 10, 1, 2, 3]]
+
+const maxPairWiseProduct = (arr) => {
+    for(let i = 0; i < arr.length; i++){
+        for(let j = 0; j < arr[i].length; j++){
+            console.log(arr[i][j])
+        }
+    }
+}
+
+maxPairWiseProduct(input)
 
