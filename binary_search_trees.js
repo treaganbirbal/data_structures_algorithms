3a5ac024b3ba83f989d9eaa169da815526a0067e
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-24 20:26:17 -0500

    add previous property to Node class

--- binary_search_trees.js
+++ binary_search_trees.js
@@ -1,9 +1,13 @@
 class Node{
-    constructor(value, next){
+    constructor(value, next, prev){
         this.value = value,
-        this.next = null
+        this.next = null,
+        this.prev = null
     }
 }
 
 let node1 = new Node(1);
-console.log(node1)
+let node2 = new Node(2);
+node1.next = node2;
+node2.prev = node1
+console.log(node2)
