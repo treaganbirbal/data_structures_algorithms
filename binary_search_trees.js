5f9b668bd578b21a4db70799f21ca1ab1492c4dd
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-24 20:36:50 -0500

    fixed nodde bug, start bst class

--- binary_search_trees.js
+++ binary_search_trees.js
@@ -1,13 +1,17 @@
 class Node{
     constructor(value){
         this.value = value,
-        this.next = null,
-        this.prev = null
+        this.left = null,
+        this.right = null
     }
 }
 
-let node1 = new Node(1);
-let node2 = new Node(2);
-node1.next = node2;
-node2.prev = node1
-console.log(node1, node2)
+class BST{
+    constructor()
+}
+
+// let node1 = new Node(1);
+// let node2 = new Node(2);
+// node1.next = node2;
+// node2.prev = node1
+// console.log(node1, node2)
