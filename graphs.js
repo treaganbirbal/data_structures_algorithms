e7eddddd1bf33ef7ba31b34d02330803f17011c8
Treagan Birbal <treaganbirbal@pursuit.org>
2020-02-27 20:45:48 -0500

    test for graph

--- graphs.js
+++ graphs.js
@@ -39,3 +39,28 @@ graph.removeEdge(2, 3);
 
 console.log(graph)
 
+
+// class Graph {
+//     constructor() {
+//       this.adjacencyList = {}
+//     }
+//     addVertex(vertex) {
+//       if (!this.adjacencyList[vertex]) this.adjacencyList[vertex] = []
+//     }
+//     addEdge(v1, v2) {
+//       this.adjacencyList[v1].push(v2)
+//       this.adjacencyList[v2].push(v1)
+//     }
+//     removeEdge(v1, v2) {
+//       this.adjacencyList[v1] = this.adjacencyList[v1].filter(v => v !== v2)
+//   ​
+//       this.adjacencyList[v2] = this.adjacencyList[v2].filter(v => v !== v1) 
+//     }
+//     removeVertex(vtx) {
+//       this.adjacencyList[vtx].forEach(v => {
+//         this.removeEdge(vtx, v);
+//       })
+//       delete this.adjacencyList[vtx]
+//     }
+// }  
+
